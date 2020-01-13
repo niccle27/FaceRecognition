84b94bf9f708286c94c71b3b0988c7f98c7d58ee
Clement Nicolas <niccle27@gmail.com>
2020-01-14 00:38:48 +0100

    update csv creation for separate test train

--- CreateDataset/create_csv.py
+++ CreateDataset/create_csv.py
@@ -4,14 +4,21 @@ import os
 import pandas as pd
 
 n_sample_per_class = 1823
-class_dic = {"Berengere": 1,
-             "Clement": 2,
-             "Edwin": 3,
-             "Pascal": 4,
-             "Ronald": 5}
+percentage_of_train = 0.8
 
-df_x = pd.DataFrame(columns=[0])
-df_y = pd.DataFrame(columns=np.arange(0, 7500))
+class_dic = {"Berengere": 0,
+             "Clement": 1,
+             "Edwin": 2,
+             "Pascal": 3,
+             "Ronald": 4,
+             "Arthur": 5}
+
+test_training_limit = int(n_sample_per_class * percentage_of_train)
+df_y_train = pd.DataFrame(columns=[0])
+df_y_test = pd.DataFrame(columns=[0])
+df_x_train = pd.DataFrame(columns=np.arange(0, 7500))
+df_x_test = pd.DataFrame(columns=np.arange(0, 7500))
+
 for dir_class in class_dic.keys():
     dir_class_path = "./Dataset/" + dir_class + "/"
     imlist = []
@@ -19,13 +26,22 @@ for dir_class in class_dic.keys():
         im_path = dir_class_path + el
         img = cv2.imread(im_path)
         imlist.append(img.flatten())
-    df_y_tmp = pd.DataFrame(imlist)
-    df_y_tmp = df_y_tmp.sample(n=n_sample_per_class)
-    xVal = np.empty(n_sample_per_class, dtype='uint8')
-    xVal.fill(class_dic[dir_class])
-    df_x_tmp = pd.DataFrame(xVal)
-    df_x = df_x.append(df_x_tmp, ignore_index=True)
-    df_y = df_y.append(df_y_tmp, ignore_index=True)
-df_x.to_csv("dataset_X.csv")
-df_y.to_csv("dataset_Y.csv")
+    df_x_tmp = pd.DataFrame(imlist)
+    df_x_tmp = df_x_tmp.sample(n=n_sample_per_class)  # shuffle all
+    df_x_tmp_train = df_x_tmp.iloc[:test_training_limit]
+    df_x_tmp_test = df_x_tmp.iloc[test_training_limit:]
+    yVal = np.empty(n_sample_per_class, dtype='uint8')
+    yVal.fill(class_dic[dir_class])
+    df_y_tmp_train = pd.DataFrame(yVal[:test_training_limit])
+    df_y_tmp_test = pd.DataFrame(yVal[test_training_limit:])
+    df_y_train = df_y_train.append(df_y_tmp_train, ignore_index=True)
+    df_y_test = df_y_test.append(df_y_tmp_test, ignore_index=True)
+    df_x_train = df_x_train.append(df_x_tmp_train, ignore_index=True)
+    df_x_test = df_x_test.append(df_x_tmp_test, ignore_index=True)
+# df_x.to_csv("dataset_X.csv")
+# df_y.to_csv("dataset_Y.csv")
+df_x_test.to_csv("df_x_test.csv")
+df_x_train.to_csv("df_x_train.csv")
+df_y_test.to_csv("df_y_test.csv")
+df_y_train.to_csv("df_y_train.csv")
 # (50, 50, 3)
